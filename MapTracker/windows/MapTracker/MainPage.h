9513fa10252319bb7a2aa04c349156fdb5b3d352
mikeagney <magney@gmail.com>
2023-11-25 15:11:16 -0800

    Check in Hello World react native app

---
+++ MapTracker/windows/MapTracker/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::MapTracker::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::MapTracker::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
